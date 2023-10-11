496b10d6c12e698e8d4fccc348108a7b9803f6dc
mummhy0811 <mummhy0811@inu.ac.kr>
2023-10-11 15:24:19 +0900

    [#4] Feat: entity 생성

---
+++ src/main/java/com/onestep/server/entity/Answer.java
@@ -0,0 +1,41 @@
+package com.onestep.server.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.annotation.Id;
+
+import java.util.Date;
+
+@ToString
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Answer {
+    @Id
+    @Column(nullable = false)
+    private Long answer_id;
+
+    //외래키
+    @JoinColumn(name="question_id")
+    private String question_id;
+
+    //외래키
+    @JoinColumn(name="user_id")
+    private String user_id;
+
+    @Column(length = 400, nullable = false)
+    private String answer_txt;
+
+    private String answer_img;
+
+    @Temporal(value = TemporalType.DATE)
+    @Column(nullable = false)
+    private Date write_date;
+
+    @ColumnDefault("0")
+    private int answer_liked;
+}
---
+++ src/main/java/com/onestep/server/entity/Family.java
@@ -0,0 +1,43 @@
+package com.onestep.server.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.annotation.Id;
+
+import java.util.Date;
+
+@ToString
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Family {
+    @Id
+    @Column(length = 100, nullable = false)
+    private String fam_id;
+
+    @Column(length = 100, nullable = false)
+    private String fam_nickname;
+
+    @ColumnDefault("0")
+    private int level;
+
+    @Column(nullable = false)
+    private Long question_id;
+
+    @ColumnDefault("true")
+    private Boolean is_valid;
+
+    @ColumnDefault("0")
+    private Long point;
+
+    @Temporal(value = TemporalType.DATE)
+    private Date fam_anniversary;
+}
