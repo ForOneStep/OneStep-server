d7614f814c19385b050158c1d8e3161f894ca0a3
mummhy0811 <mummhy0811@inu.ac.kr>
2023-10-17 18:49:44 +0900

    [#28 #29]Feat: 공통질문 배포 및 READ API 생성

--- src/main/java/com/onestep/server/repository/IFamilyRepository.java
+++ src/main/java/com/onestep/server/repository/IFamilyRepository.java
@@ -2,6 +2,12 @@ package com.onestep.server.repository;
 
 import com.onestep.server.entity.Family;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 
 public interface IFamilyRepository extends JpaRepository<Family,String> {
+
+    @Query("UPDATE Family f SET f.question_id = f.question_id+1 , f.is_valid = false  WHERE f.is_valid=true")
+    void changeQuestionAndState();
+
 }
