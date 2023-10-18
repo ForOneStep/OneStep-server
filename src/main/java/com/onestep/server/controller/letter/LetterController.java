9383393fa724e44b561cc0c87633fcbdd3f83cc5
mummhy0811 <mummhy0811@inu.ac.kr>
2023-10-18 16:17:41 +0900

    [#37]Feat: 이번주 공개된 쪽지 Read

--- src/main/java/com/onestep/server/controller/letter/LetterController.java
+++ src/main/java/com/onestep/server/controller/letter/LetterController.java
@@ -15,22 +15,28 @@ import java.util.List;
 public class LetterController {
     private final LetterService letterService;
 
-    //익명 편지 작성
+    //익명 쪽지 작성
     @PostMapping(value = "/letter/write")
     public Letter writeLetter(@RequestBody Letter letter){
         letterService.writeLetter(letter);
         return letter;
     }
 
-    //내가 작성한 편지 확인
+    //내가 작성한 쪽지 확인
     @GetMapping(value = "/letter/byUser/{userId}")
     public List<LetterListDTO> findLetterByWriterId(@PathVariable String userId){
         return letterService.findLetterByWriterId(userId);
     }
 
-    //가족 아이디로 편지 목록 확인
+    //가족 아이디로 쪽지 목록 확인
     @GetMapping(value = "/letter/byFamily/{familyId}")
     public List<LetterListDTO> findLetterByFamilyId(@PathVariable String familyId){
         return letterService.findLetterByFamilyId(familyId);
+    }
+
+    //가족 아이디로 이번주 공개된 쪽지 확인
+    @GetMapping(value = "/letter/weekly/{familyId}")
+    public List<LetterListDTO> findWeeklyLetterByFamilyId(@PathVariable String familyId){
+        return letterService.findWeeklyLetterByFamilyId(familyId);
     }
 }
--- src/main/java/com/onestep/server/repository/ILetterRepository.java
+++ src/main/java/com/onestep/server/repository/ILetterRepository.java
@@ -11,13 +11,17 @@ import java.util.List;
 
 
 public interface ILetterRepository extends JpaRepository<Letter, Long> {
-    //특정 사람이 작성한 익명 편지 리스트 확인
+    //특정 사람이 작성한 익명 쪽지 리스트 확인
     @Query("SELECT l FROM Letter l WHERE l.user =:user")
     List<Letter> findLetterByWriterId(@Param("user") User user);
 
-    //가족 아이디로 편지 목록 확인
-    @Query("SELECT l, u FROM Letter l LEFT JOIN User u ON l.user = u WHERE u.family =:family")
+    //가족 아이디로 과거 쪽지 목록 확인
+    @Query("SELECT l, u FROM Letter l LEFT JOIN User u ON l.user = u WHERE u.family =:family and l.letter_state>0")
     List<Letter> findLetterByFamilyId(@Param("family") Family family);
+
+    //가족 아이디로 이번주 공개된 쪽지 목록 확인
+    @Query("SELECT l, u FROM Letter l LEFT JOIN User u ON l.user = u WHERE u.family =:family and l.letter_state=1")
+    List<Letter> findWeeklyLetterByFamilyId(@Param("family") Family family);
 
     //쪽지 상태 변경
     @Query("UPDATE Letter l SET l.letter_state = l.letter_state+1  WHERE l.letter_state<2")
