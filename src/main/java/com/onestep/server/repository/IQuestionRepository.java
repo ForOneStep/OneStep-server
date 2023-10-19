adfc2611b3839d51b85275997154c03805286c1d
mummhy0811 <mummhy0811@inu.ac.kr>
2023-10-19 14:34:46 +0900

    [#39]Feat: 답변한 질문 목록 Read API 생성

--- src/main/java/com/onestep/server/repository/IQuestionRepository.java
+++ src/main/java/com/onestep/server/repository/IQuestionRepository.java
@@ -1,8 +1,15 @@
 package com.onestep.server.repository;
 
-import com.onestep.server.entity.Letter;
 import com.onestep.server.entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface IQuestionRepository extends JpaRepository<Question, Long> {
+
+    //질문 목록 확인
+    @Query("SELECT q FROM Question q WHERE q.question_id <=:questionId")
+    List<Question> findListById(@Param("questionId") Long questionId);
 }
