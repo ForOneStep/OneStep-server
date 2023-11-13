5d10b463dd7cd69ae4e3688132da53c59385a651
minyeong <106754293+miinyeong@users.noreply.github.com>
2023-11-13 21:25:09 +0900

    Feat: QuizAnswer 테이블 생성

---
+++ src/main/java/com/onestep/server/entity/QuizAnswer.java
@@ -0,0 +1,37 @@
+package com.onestep.server.entity;
+
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+
+@ToString
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class QuizAnswer {
+
+    @Id
+    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long quizAnswer_id;
+
+    @ManyToOne
+    @JoinColumn(name="quiz_id")
+    private Quiz quiz;
+
+    //외래키
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private User user;
+
+    private Integer quiz_ans;
+
+    @ColumnDefault("2") //0 오답, 1 정답
+    private Integer quiz_state;
+
+
+}
