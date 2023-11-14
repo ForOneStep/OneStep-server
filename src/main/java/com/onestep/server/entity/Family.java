e9ad387397f0e9ef64c84aea4aca66c072504c4b
mummhy0811 <mummhy0811@inu.ac.kr>
2023-11-14 20:03:59 +0900

    [#76]Feat: 유저 정보 Read

--- src/main/java/com/onestep/server/entity/Family.java
+++ src/main/java/com/onestep/server/entity/Family.java
@@ -1,6 +1,5 @@
 package com.onestep.server.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Temporal;
@@ -29,14 +28,11 @@ public class Family {
     @ColumnDefault("0")
     private int level;
 
-    @Column(nullable = false)
-    private Long question_id;
-
     @ColumnDefault("true")
     private Boolean is_valid;
 
-    @ColumnDefault("0")
-    private Long point;
+    @Column(nullable = false)
+    private int fam_number;
 
     @Temporal(value = TemporalType.DATE)
     private Date fam_anniversary;
