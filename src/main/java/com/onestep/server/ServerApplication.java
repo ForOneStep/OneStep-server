06e001b704877811e0122db895ea5c1042503a41
mummhy0811 <mummhy0811@inu.ac.kr>
2023-11-16 14:05:08 +0900

    Feat: set default timezone to seoul

--- src/main/java/com/onestep/server/ServerApplication.java
+++ src/main/java/com/onestep/server/ServerApplication.java
@@ -4,11 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableScheduling
 @SpringBootApplication
 public class ServerApplication {
 	static {
 		System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+	}
+
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
